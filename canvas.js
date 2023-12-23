270850dd5cebbaa3fda35ef6fc9deda5c3f90ea9
AntJamGeo <57462383+AntJamGeo@users.noreply.github.com>
2023-12-23 10:41:09 +0000

    Draw random rectangles

--- canvas.js
+++ canvas.js
@@ -2,3 +2,16 @@ const canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const c = canvas.getContext('2d');
+
+const maxSize = 100;
+
+for (let i = 0; i < 10; i++) {
+  c.fillRect(
+    Math.random()*(window.innerWidth-maxSize),
+    Math.random()*(window.innerHeight-maxSize),
+    Math.random()*maxSize, 
+    Math.random()*maxSize
+  );
+}
